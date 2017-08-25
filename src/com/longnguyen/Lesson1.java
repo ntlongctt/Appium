b94c9df74adab05581a06f34dcb02c03b4f95563
longnguyen <thanhlong10119@gmail.com>
2017-08-25 21:58:33 +0700

    update class name

--- src/com/longnguyen/Lesson1.java
+++ src/com/longnguyen/Lesson1.java
@@ -6,12 +6,13 @@ import java.util.concurrent.TimeUnit;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class Leson1 extends Base {
-	public static void main(String[] args) throws MalformedURLException {
+public class Lesson1 extends Base {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		AndroidDriver<AndroidElement> driver = setCapability();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
-		
+		Thread.sleep(2000);
+		driver.quit();
 		System.out.println("Done!");
 	}
 }
